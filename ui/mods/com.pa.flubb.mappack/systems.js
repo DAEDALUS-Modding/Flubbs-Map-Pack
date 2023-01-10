2f465e5c2ff6e941c800d95512eac447fd2b282c
quildtide <42811940+quildtide@users.noreply.github.com>
2023-01-09 19:39:20 -0500

    Fix references to mod id

--- ui/mods/com.pa.flubb.mappack/systems.js
+++ ui/mods/com.pa.flubb.mappack/systems.js
@@ -1,11 +1,11 @@
-api.file.list('/client_mods/com.pa.flubb.mappack/ui/mods/com.flubbateios.mappack/flubb/').then(function (r) {
+api.file.list('/client_mods/com.pa.flubb.mappack/ui/mods/com.pa.flubb.mappack/flubb/').then(function (r) {
 	var maps = [];
 	for (var x in r) {
 		maps.push(r[x].replace('/client_mods/com.pa.flubb.mappack/', 'coui://'));
 	}
 	cShareSystems.load_pas("flubb's Tremendous Map Pack", maps);
 });
-api.file.list('/client_mods/com.flubbateios.mappack/ui/mods/com.pa.flubb.mappack/nik/').then(function (r) {
+api.file.list('/client_mods/com.pa.flubb.mappack/ui/mods/com.pa.flubb.mappack/nik/').then(function (r) {
 	var maps = [];
 	for (var x in r) {
 		maps.push(r[x].replace('/client_mods/com.pa.flubb.mappack/', 'coui://'));
